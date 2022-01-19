979c87a89230169e066ad21b72876b783ced4041
ASHOK <ashoktut.2@gmail.com>
2022-01-19 02:03:33 +0200

    Home Page

---
+++ src/app/pages/tabs/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  slideOptions = {
+    slidesPerView: 1.1,
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
