ae6a84d119c4004d88772d3a0ed1d2b7a1b92b5c
ASHOK <ashoktut.2@gmail.com>
2022-01-26 21:02:19 +0200

    functional cart

--- src/app/pages/tabs/cart/cart.page.ts
+++ src/app/pages/tabs/cart/cart.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Storage } from "@capacitor/storage";
+import { IonContent } from '@ionic/angular';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-cart',
@@ -9,11 +11,13 @@ import { Storage } from "@capacitor/storage";
 })
 export class CartPage implements OnInit {
 
+  @ViewChild(IonContent, {static: false}) content: IonContent; // use viewchild in order to access the ion-content section in the html for the view bill scrolling functionality. content is a variable.
   urlCheck: any;
   url: any;
   model: any = {};
   deliveryCharge = 20;
   instruction: any;
+  location: any = {};
 
   constructor(
     private router: Router
@@ -21,15 +25,20 @@ export class CartPage implements OnInit {
 
   ngOnInit() {
     this.checkUrl();
-    this.getCartData();
+    this.getModel();
   }
 
   getCart() {
     return Storage.get({key: 'cart'});
   }
 
-  async getCartData() {
+  async getModel() {
     let data: any = await this.getCart();
+    this.location = {
+      lat: 28.653831,
+      lng: 77.188257,
+      address: 'Karol Bagh, New Delhi'
+    };
     if(data?.value) {
       this.model = await JSON.parse(data.value);
       console.log(this.model);
@@ -42,14 +51,14 @@ export class CartPage implements OnInit {
     this.model.items = item;
     this.model.totalPrice = 0;
     this.model.totalItem = 0;
-    this.model.deliveryCharge = 0;
+    this.model.deliveryCharge = 20;
     this.model.grandTotal = 0;
 
     item.forEach(element => {
       this.model.totalItem += element.quantity;
       this.model.totalPrice += (parseFloat(element.price) * parseFloat(element.quantity));
     });
-    this.model.deliveryCharge = this.deliveryCharge;
+    this.model.deliveryCharge = parseFloat(this.model.deliveryCharge).toFixed(2);
     this.model.totalPrice = parseFloat(this.model.totalPrice).toFixed(2);
     this.model.grandTotal = (parseFloat(this.model.totalPrice) + parseFloat(this.model.deliveryCharge)).toFixed(2);
     if(this.model.totalItem == 0) {
@@ -83,11 +92,27 @@ clearCart() {
   }
 
   quantityPlus(index) {
-
+    try {
+      console.log(this.model.items[index]);
+      if (!this.model.items[index].quantity || this.model.items[index].quantity === 0) {
+        this.model.items[index].quantity = 1;
+        this.calculate();
+      } else {
+        this.model.items[index].quantity += 1;
+        this.calculate();
+      }
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   quantityMinus(index) {
-
+    if(this.model.items[index].quantity !== 0) {
+      this.model.items[index].quantity -= 1;
+    } else {
+      this.model.items[index].quantity = 0;
+    }
+    this.calculate();
   }
 
   addAddress() {
@@ -99,6 +124,26 @@ clearCart() {
   }
 
   makePayment() {
-    console.log('make payment');
+    try {
+      const data = {
+        restaurant_id: this.model.restaurant.uid,
+        res: this.model.restaurant,
+        order: JSON.stringify(this.model.items),
+        time: moment().format('lll'),
+        address: this.location,
+        total: this.model.totalPrice,
+        grandTotal: this.model.grandTotal,
+        deliveryCharge: this.deliveryCharge,
+        status: 'Created',
+        paid: 'COD'
+      };
+      console.log('order: ', data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  scrollToBottom() {
+    this.content.scrollToBottom(500);
   }
 }
